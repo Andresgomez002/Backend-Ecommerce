1f83c5644f84ab483e7478382d0a0fae8522b098
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-06-28 20:29:57 -0500

    [+] app.ts: Define 2 endpoints, /, /home

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,23 @@ import express from 'express';
 
 const app = express();
 
+/** Routing: Enrutamiento */
+// http://localhost:3000/       (EndPoint)
+app.get( '/', ( req, res ) => {
+    const message = 'Bienvenido a la API de Ecommerce';
+
+    console.log( message );     // Mensaje a la terminal
+    res.send( `<h1>${ message }</h1>` );        // Mensaje al navegador
+});
+
+// http://localhost:3000/home   (EndPoint)
+app.get( '/home', ( req, res ) => {
+    const namePage = 'Home';
+
+    console.log( namePage );
+    res.send( `<h1>${ namePage }</h1>` );
+});
+
 app.listen( 3000, () => {
     console.log( 'Servidor lanzado en el puerto 3000' );
 });
