ae73a97d24c6fe3a3f20e1a7557452cd992ed13b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-06-30 20:29:40 -0500

    [+] products.model: Define estrutura de la entidad (Modelo BD)

---
+++ src/models/products.model.ts
@@ -0,0 +1,31 @@
+/** La estructura que definamos aqui se vera reflejada en nuestra base de datos */
+import { Schema, model } from "mongoose";
+
+
+/** Define estructura de datos en la base de datos requerida por Mongoose*/
+const productSchema = new Schema(
+    // Objeto principal definira atributos del modelo
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: false
+        },
+        description: {
+            type: String
+        }
+    },
+    // Definira configuraciones que se pueden aplicar en Mongoose para ese objeto
+    {
+        timestamps: true
+    }
+);
+
+/** Define el Modelo a partir de la estructura requerida por Mongoose */
+const productModel = model( 'Products', productSchema  );
+
+
+export default productModel;
