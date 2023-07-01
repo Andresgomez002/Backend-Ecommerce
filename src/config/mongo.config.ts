7e95164b8e8ef130b6a9e33e02ac918365747f17
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-06-30 21:37:25 -0500

    [+] mongo.config: Agregamos al URI de Mongo el nombre de la base de datos

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 // Configuracion para la conexion con MongoDB
 async function dbConnect() : Promise<void> {
-    const DB_URI = 'mongodb://127.0.0.1:27017';
+    const DB_URI = 'mongodb://127.0.0.1:27017/store';
     await connect( DB_URI );
 }
 
