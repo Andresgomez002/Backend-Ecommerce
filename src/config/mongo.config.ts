73867eea43e3d14658b8872d2d769524c97dc392
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-06-30 21:51:17 -0500

    [+] mongo.config: Define variable de entorno para URI a MongoDB [+] .env.example

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 // Configuracion para la conexion con MongoDB
 async function dbConnect() : Promise<void> {
-    const DB_URI = 'mongodb://127.0.0.1:27017/store';
+    const DB_URI = `${ process.env.DB_URI }`;     // <string> 
     await connect( DB_URI );
 }
 
